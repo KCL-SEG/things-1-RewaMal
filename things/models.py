2a3505485ed6ede9e32a149aa6f1056d22815bb5
rewamal <rewa.mal@kcl.ac.uk>
2022-10-14 12:45:51 +0100

    edit import statement in models.py

--- things/models.py
+++ things/models.py
@@ -1,6 +1,4 @@
-
 from django.db import models
-from django.contrib.auth.models import AbstractUser
 from django.core.validators import MaxValueValidator, MinValueValidator 
 
 # Create your models here.
