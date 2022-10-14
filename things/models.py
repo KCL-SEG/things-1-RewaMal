b6667f94d6a51bdd2100d9e5519f75fc5d7c9ddc
rewamal <rewa.mal@kcl.ac.uk>
2022-10-14 12:29:27 +0100

    remove a wrong import statment in models.py

--- things/models.py
+++ things/models.py
@@ -1,4 +1,4 @@
-from msilib.schema import Class
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import MaxValueValidator, MinValueValidator 
