130a9082c78826c3e79026be831fe97c834de5fe
rewamal <rewa1422@hotmail.com>
2022-11-20 19:27:40 +0000

    delete some attributes

--- things/models.py
+++ things/models.py
@@ -10,11 +10,9 @@ class Thing(models.Model):
     )
     description = models.CharField(
         max_length = 120,
-        unique = False,
         blank = True,
     )
     quantity = models.IntegerField(
-        unique = False,
         validators= [
             MinValueValidator(0),
             MaxValueValidator(100)
